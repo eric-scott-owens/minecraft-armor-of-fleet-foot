9d41fc0bfc202b1b65376b3078b231d50f73feac
Scott McKye Owens <Scott.McKye.Owens@hotmail.com>
2024-01-17 19:37:01 -0700

    Add Base Fabric Libraries

---
+++ src/main/java/net/owens/armor_of_fleet_foot/ArmorofFleetFootDataGenerator.java
@@ -0,0 +1,11 @@
+package net.owens.armor_of_fleet_foot;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class ArmorofFleetFootDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
