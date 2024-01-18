7bd69237602889fbc6de800f3a367e10dffac985
Scott McKye Owens <Scott.McKye.Owens@hotmail.com>
2024-01-17 20:03:23 -0700

    Renamed Files and classes ArmorOfFleetFoot, ArmorOfFleetFootDataGenerator

--- src/main/java/net/owens/armor_of_fleet_foot/ArmorofFleetFootDataGenerator.java
+++ src/main/java/net/owens/armor_of_fleet_foot/ArmorofFleetFootDataGenerator.java
@@ -3,7 +3,7 @@ package net.owens.armor_of_fleet_foot;
 import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
 import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
 
-public class ArmorofFleetFootDataGenerator implements DataGeneratorEntrypoint {
+public class ArmorOfFleetFootDataGenerator implements DataGeneratorEntrypoint {
 	@Override
 	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
 
