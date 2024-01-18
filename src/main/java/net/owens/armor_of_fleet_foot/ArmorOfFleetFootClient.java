bc6797548631a35df127c54aec38e9f1ea6559fe
Scott McKye Owens <Scott.McKye.Owens@hotmail.com>
2024-01-17 20:34:53 -0700

    Edited ArmorOfFleetFootClient.java and fabric.mod.json and gradle.properties

---
+++ src/main/java/net/owens/armor_of_fleet_foot/ArmorOfFleetFootClient.java
@@ -0,0 +1,10 @@
+package net.owens.armor_of_fleet_foot;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class ArmorOfFleetFootClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
